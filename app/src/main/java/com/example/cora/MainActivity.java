e4e064714a1ab8e5b59ee9c2d28f5c08b4a96c0f
deonnao <deonnaowens3@yahoo.com>
2023-02-25 03:23:40 -0600

    added code, text, and edit ui.

--- app/src/main/java/com/example/cora/MainActivity.java
+++ app/src/main/java/com/example/cora/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.cora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button codeBtn = findViewById(R.id.codeBtn);
+        Button textBtn = findViewById(R.id.textBtn);
+        Button editBtn = findViewById(R.id.editBtn);
+
+        codeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CodeEditor.class));
+            }
+        });
+
+        textBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Chat.class));
+            }
+        });
+
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, EditActivity.class));
+            }
+        });
     }
 }

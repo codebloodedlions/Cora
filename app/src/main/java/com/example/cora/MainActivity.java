e179a1ecc3972e9bf2fc209f1c710cd74c803a4b
deonnao <deonnaowens3@yahoo.com>
2023-02-25 05:12:17 -0600

    added code, text, and edit ui.

--- app/src/main/java/com/example/cora/MainActivity.java
+++ app/src/main/java/com/example/cora/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.example.cora.userInterface.CodeEditor;
+import com.example.cora.userInterface.EditActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 

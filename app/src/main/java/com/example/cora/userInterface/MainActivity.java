2614a39ac52b0f87831c63c5d009e49e6d74b019
Charles Lett Jr <cclett2000@gmail.com>
2023-02-25 20:43:03 -0600

    [C.Lett - BACKEND] fixed UI bug

--- app/src/main/java/com/example/cora/userInterface/MainActivity.java
+++ app/src/main/java/com/example/cora/userInterface/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTransitionCompleted(MotionLayout motionLayout, int currentId) {
-                //startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
             }
 
             @Override
